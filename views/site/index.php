ec0219a6b9c8d7ba20797758779b19617872e6b8
mix <mixartemev@gmail.com>
2017-09-23 15:40:28 +0300

    !!

--- views/site/index.php
+++ views/site/index.php
@@ -7,7 +7,7 @@ $this->title = Yii::$app->name;
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Origins!</h1>
+        <h1>Origins!!</h1>
 
         <p class="lead">Our mission:</p>
         <p>Build a tree of all words in all languages with direction of origin,
